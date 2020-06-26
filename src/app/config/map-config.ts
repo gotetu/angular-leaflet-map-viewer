868264b9ad5e5558156134c3f03b4746affbaa78
Tomoyuki HIRAI <gotetu@gmail.com>
2020-06-26 23:58:31 +0900

    微修正

--- src/app/config/map-config.ts
+++ src/app/config/map-config.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 const mapConfig : MapConfig = {
     center: [35.5766335,139.6572773],
     urlTemplate: 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
-    attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
+    attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院ラスタ</a>',
 };
 
 @Injectable({
