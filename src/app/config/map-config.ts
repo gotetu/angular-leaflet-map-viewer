881bf17e83d38ea9705d06db27bac290a4ee30dc
Tomoyuki HIRAI <gotetu@gmail.com>
2020-06-30 22:38:24 +0900

    背景地図の切り替え対応 close #2

--- src/app/config/map-config.ts
+++ src/app/config/map-config.ts
@@ -2,9 +2,24 @@ import { Injectable } from '@angular/core';
 
 const mapConfig : MapConfig = {
     center: [35.5766335,139.6572773],
-    layerNames: ['地理院地図 標準地図','地理院地図 淡色地図',],
-    urlTemplates: ['https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png','https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png',],
-    attributions: ['<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院ラスタ</a>','<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院ラスタ</a>',],
+    layerNames: [
+        '地理院地図 標準地図',
+        '地理院地図 淡色地図',
+        '地理院地図 白地図',
+        '地理院地図 写真',
+    ],
+    urlTemplates: [
+        'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
+        'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png',
+        'https://cyberjapandata.gsi.go.jp/xyz/blank/{z}/{x}/{y}.png',
+        'https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg',
+    ],
+    attributions: [
+        '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
+        '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
+        '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
+        '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
+    ],
 };
 
 @Injectable({
