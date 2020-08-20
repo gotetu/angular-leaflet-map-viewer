5daf481727cdbd2b4c882707e82381bc3bc102e5
Tomoyuki HIRAI <gotetu@gmail.com>
2020-08-21 06:36:23 +0900

    ソース整形.

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 
-import { MapConfig } from 'src/app/config/map-config';
-import { MarkerService } from 'src/app/services/marker/marker.service';
+import {MapConfig} from 'src/app/config/map-config';
+import {MarkerService} from 'src/app/services/marker/marker.service';
 
 import {EventPage} from 'src/app/services/marker/eventmap.city.kawasaki.events.model';
 
@@ -11,15 +11,15 @@ import {EventPage} from 'src/app/services/marker/eventmap.city.kawasaki.events.m
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-  map:any;
+export class MapComponent implements OnInit, AfterViewInit {
+  map: any;
   center: number[];
   layerNames: string[];
   urlTemplates: string[];
   attributions: string[];
   @Input() mapId = 'mapid';
-  constructor({center, layerNames, urlTemplates, attributions} : MapConfig,
-    private marker: MarkerService) {
+  constructor({center, layerNames, urlTemplates, attributions}: MapConfig,
+              private marker: MarkerService) {
     this.center = center;
     this.layerNames = layerNames;
     this.urlTemplates = urlTemplates;
@@ -30,7 +30,7 @@ export class MapComponent implements OnInit {
     const iconRetinaUrl = 'src/assets/marker-icon-2x.png';
     const iconUrl = 'src/assets/marker-icon.png';
     const shadowUrl = 'src/assets/marker-shadow.png';
-    const iconDefault = L.icon({
+    L.Marker.prototype.options.icon = L.icon({
       iconRetinaUrl,
       iconUrl,
       shadowUrl,
@@ -40,7 +40,6 @@ export class MapComponent implements OnInit {
       tooltipAnchor: [16, -28],
       shadowSize: [41, 41]
     });
-    L.Marker.prototype.options.icon = iconDefault;
   }
   ngAfterViewInit() {
     this.initMap();
@@ -48,12 +47,12 @@ export class MapComponent implements OnInit {
   }
 
   /**
-   * ’n}‰Šúİ’è.
+   * åœ°å›³åˆæœŸè¨­å®š.
    */
   initMap() {
     this.map = L.map(this.mapId, {
       zoomControl: false
-    }).setView([this.center[0],this.center[1]], 14);
+    }).setView([this.center[0], this.center[1]], 14);
 
     this.initLayer();
     this.initBaseLayer();
@@ -61,7 +60,7 @@ export class MapComponent implements OnInit {
   }
 
   /**
-   * ‰Šú•\¦ƒŒƒCƒ„‚ğİ’è‚·‚é.
+   * åˆæœŸè¡¨ç¤ºãƒ¬ã‚¤ãƒ¤ã‚’è¨­å®šã™ã‚‹.
    */
   initLayer() {
     if (this.urlTemplates.length > 0) {
@@ -72,40 +71,40 @@ export class MapComponent implements OnInit {
   }
 
   /**
-   * ƒx[ƒXƒŒƒCƒ„‚ğİ’è‚·‚é.
+   * ãƒ™ãƒ¼ã‚¹ãƒ¬ã‚¤ãƒ¤ã‚’è¨­å®šã™ã‚‹.
    */
   initBaseLayer() {
-    let layerControl = L.control.layers(null,null,{position:"topleft"})
+    const layerControl = L.control.layers(null, null, {position: 'topleft'});
     for (let i = 0; i < this.urlTemplates.length; i++) {
       layerControl.addBaseLayer(
         L.tileLayer(this.urlTemplates[i], {
           attribution: this.attributions[i]
-        }),this.layerNames[i]);
+        }), this.layerNames[i]);
     }
     layerControl.addTo(this.map);
   }
 
   /**
-   * ’n}ƒŒƒCƒAƒEƒg‚ğİ’è‚·‚é.
+   * åœ°å›³ãƒ¬ã‚¤ã‚¢ã‚¦ãƒˆã‚’è¨­å®šã™ã‚‹.
    */
   initMapLayout() {
-    // ƒXƒP[ƒ‹•\¦iƒtƒB[ƒg•\‹LFOFFj
-    L.control.scale({imperial:false}).addTo(this.map);
-    // ƒY[ƒ€ƒRƒ“ƒgƒ[ƒ‹
-    L.control.zoom({position:"bottomleft"}).addTo(this.map);
+    // ã‚¹ã‚±ãƒ¼ãƒ«è¡¨ç¤ºï¼ˆãƒ•ã‚£ãƒ¼ãƒˆè¡¨è¨˜ï¼šOFFï¼‰
+    L.control.scale({imperial: false}).addTo(this.map);
+    // ã‚ºãƒ¼ãƒ ã‚³ãƒ³ãƒˆãƒ­ãƒ¼ãƒ«
+    L.control.zoom({position: 'bottomleft'}).addTo(this.map);
   }
 
   /**
-   * ƒCƒxƒ“ƒgî•ñ‚ğƒvƒƒbƒg‚·‚é.
+   * ã‚¤ãƒ™ãƒ³ãƒˆæƒ…å ±ã‚’ãƒ—ãƒ­ãƒƒãƒˆã™ã‚‹.
    * @remakrs
-   * –{—ˆ‚Å‚ ‚ê‚ÎMapComponents‚Ìˆ—‚Å‚Í‚È‚¢‚ªAb’è“I‚ÉB
+   * æœ¬æ¥ã§ã‚ã‚Œã°MapComponentsã®å‡¦ç†ã§ã¯ãªã„ãŒã€æš«å®šçš„ã«ã€‚
    */
   plotEventPage(eventPage: EventPage) {
     for (const event of eventPage.event_data) {
       L.marker([event.place_lat, event.place_lon],
         {title: `${event.title}`}).addTo(this.map)
         .bindPopup(`${event.title}<br/>${event.content}`);
-        /* html ‚Ìƒeƒ“ƒvƒŒ[ƒg‹@”\‚ğg‚¦‚Î\‘¢‰»‚³‚ê‚½î•ñ‚ğŠy‚É•\¦‚Å‚«‚é‚©‚à‚µ‚ê‚È‚¢ */
+        /* html ã®ãƒ†ãƒ³ãƒ—ãƒ¬ãƒ¼ãƒˆæ©Ÿèƒ½ã‚’ä½¿ãˆã°æ§‹é€ åŒ–ã•ã‚ŒãŸæƒ…å ±ã‚’æ¥½ã«è¡¨ç¤ºã§ãã‚‹ã‹ã‚‚ã—ã‚Œãªã„ */
     }
   }
 
